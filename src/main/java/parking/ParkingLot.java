210f66a015a69ce346e54abf8b6754089d1b83d9
Ganesh Sawant <ganesh.sawant@Ganeshs-MacBook-Pro.local>
2023-09-13 12:32:33 +0530

    Added availableSlots to maintain original capacity untouched

--- src/main/java/parking/ParkingLot.java
+++ src/main/java/parking/ParkingLot.java
@@ -3,16 +3,18 @@ package parking;
 import java.util.HashSet;
 
 public class ParkingLot {
-    private int capacity;
+    private final int capacity;
+    private int availableSlots;
     private final HashSet<String> parkedVehicles = new HashSet<String>();
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        this.availableSlots = capacity;
     }
 
     public boolean park(String carIdentifier) {
-        if (capacity > 0) {
-            capacity--;
+        if (availableSlots > 0) {
+            availableSlots--;
             parkedVehicles.add(carIdentifier);
             return true;
         }
@@ -25,5 +27,9 @@ public class ParkingLot {
 
     public void unPark(String carIdentifier) {
         parkedVehicles.remove(carIdentifier);
+        availableSlots++;
+        if (parkedVehicles.isEmpty()) {
+            availableSlots = this.capacity;
+        }
     }
 }
